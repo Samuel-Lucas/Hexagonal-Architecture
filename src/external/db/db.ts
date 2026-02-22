7a7e2e110e04614070d8965fc20456b3ac355afc
Samuel-Lucas <samuel.moreira10@outlook.com>
2026-02-22 16:38:41 -0300

    Configuring postgres to persist user in database

--- src/external/db/db.ts
+++ src/external/db/db.ts
@@ -1,13 +1,16 @@
-import pgPromise from "pg-promise";
+import 'dotenv/config'
+import pgPromise from "pg-promise"
 
 const pgp = pgPromise()
+
+console.log("Conectando com o usuário:", process.env.DB_USER)
 
 const db = pgp({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: String(process.env.DB_PASSWORD),
 })
 
 export default db
